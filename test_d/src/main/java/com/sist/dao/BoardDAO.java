b1713909a4863c7c80e400de07fdcfecfe4f84ab
sisttest03 <rola70001@gmail.com>
2021-12-24 10:27:10 +0900

    BoardDAO 구현

---
+++ test_d/src/main/java/com/sist/dao/BoardDAO.java
@@ -0,0 +1,12 @@
+package com.sist.dao;
+
+public class BoardDAO {
+		public void insert() {
+			System.out.println("게시물을 등록하였습니다.");
+		}
+		
+		public void update() {
+			System.out.println("게시물을 수정하였습니다.");
+		}
+
+}
