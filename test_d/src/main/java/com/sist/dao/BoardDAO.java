1d505c04788f33f55ab6a8892525a0af75e5abfc
user <user@192.168.10.64>
2021-12-24 10:39:22 +0900

    BoardDAO, GoodsDAO

---
+++ test_d/src/main/java/com/sist/dao/BoardDAO.java
@@ -0,0 +1,14 @@
+package com.sist.dao;
+
+public class BoardDAO {
+	public int insert(){
+		System.out.println("게시물을 등록하였습니다.");
+		return 1;
+	}
+	public int update(){
+		System.out.println("게시물을 수정하였습니다.");
+		return 1;
+	}
+}
+
+	
---
+++ test_d/src/main/java/com/sist/dao/GoodsDAO.java
@@ -0,0 +1,14 @@
+package com.sist.dao;
+
+public class GoodsDAO {
+	
+	public void insert() {
+		System.out.println("등록하였습니다.");
+		
+	}
+	public void update() {
+		System.out.println("수정하였습니다.");
+		
+	}
+
+}
